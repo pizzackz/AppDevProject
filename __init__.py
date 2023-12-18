35d009facb8d11c145886651a2ce1127a37d767f
Unknown2348 <ongzhaohan03@gmail.com>
2023-12-18 16:02:15 +0800

    Added 3 home pages
    Created home page views for guests, admins and customers as well as did routing to those html templates. Subject to change in future

--- __init__.py
+++ __init__.py
@@ -2,10 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Guest home page (Default)
 @app.route("/")
-@app.route("/home")
 def home():
-    return render_template("base.html")
+    return render_template("home.html")
+
+
+# Customer home page
+@app.route("/customer/")
+def customer_home():
+    return render_template("customer_home.html")
+
+
+# Admin home page
+@app.route("/admin/")
+def admin_home():
+    return render_template("admin_home.html")
 
 
 if __name__ == "__main__":
