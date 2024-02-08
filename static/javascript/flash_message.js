86d1a0d2b7ff4e148f5aba40270acaf64be6e206
typingWraith <100477027+typingWraith@users.noreply.github.com>
2024-02-08 15:21:56 +0800

    Add files via upload

---
+++ static/javascript/flash_message.js
@@ -0,0 +1,49 @@
+function display_popup(message, status) {
+    var popup = document.getElementById('popup');
+    if (status == 'info') {
+      popup.innerHTML = `
+      <span class="bi bi-info-circle" id="exclamation"></span>
+      <span class="msg">` + message + `</span>
+      <div id="close-btn">
+        <span class="bi bi-x"></span>
+      </div>
+    `;
+       popup.style.backgroundColor = "rgb(48, 75, 134)";
+       console.log('Info')
+    }
+    else if (status == 'error') {
+      popup.innerHTML = `
+      <span class="bi bi-exclamation-circle" id="exclamation"></span>
+      <span class="msg">Error: ` + message + `</span>
+      <div id="close-btn">
+        <span class="bi bi-x"></span>
+      </div>
+    `;
+       popup.style.backgroundColor = "rgb(135,41,41)";
+
+    }
+
+    else if (status == 'success') {
+      popup.innerHTML = `
+      <span class="bi bi-check-lg" id="exclamation"></span>
+      <span class="msg">` + message + `</span>
+      <div id="close-btn">
+        <span class="bi bi-x"></span>
+      </div>
+    `;
+       popup.style.backgroundColor = "rgb(61, 92, 52)";
+       console.log('Success')
+    }
+    popup.classList.remove('hide');
+    popup.classList.add('show1');
+
+    var close_btn = document.getElementById('close-btn');
+    setTimeout(hide_popup, 9000);
+    close_btn.addEventListener('click', hide_popup);
+}
+
+function hide_popup() {
+    var popup = document.getElementById('popup');
+    popup.classList.remove('show1');
+    popup.classList.add('hide');
+}
