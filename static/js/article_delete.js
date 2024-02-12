e4cc2ad6810ad9ee7603f0d553b68323235ee2a5
Miguel <animatorpizzaguy74@gmail.com>
2024-02-12 21:27:09 +0800

    Changes to Jacen's code and setup the readme file

--- static/js/article_delete.js
+++ static/js/article_delete.js
@@ -3,6 +3,6 @@ function confirmdelete(content, id) {
     console.log(content);
     document.getElementById('modal-body').innerHTML = 'Are you sure you want to delete ' + content + '?';
     document.getElementById('delete').onclick = function() {
-        window.location.href = "/delete_article/" + id;
+        window.location.href = "delete_article/" + id;
     }
 }
